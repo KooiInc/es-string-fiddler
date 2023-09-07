ff0f84cdcf39c33d5a54518cea63239b37b94c8d
Renzo Kooi <renzo.kooi@gmail.com>
2023-09-07 12:09:25 +0200

    b&b (readability, cryptocheck)

--- Resource/randomStringFactory.js
+++ Resource/randomStringFactory.js
@@ -2,18 +2,17 @@ const { randomString, uuid4 } = randomStringGeneratorFactory();
 export { randomString as default, randomString, uuid4 };
 
 function randomStringGeneratorFactory() {
-  const getRandomValue = () => window.crypto
+  const hasCrypto = checkBrowserCrypto();
+  const getRandomValue = () => hasCrypto
     ? [...crypto.getRandomValues(new Uint32Array(1))].shift() / 2**32
     : Math.random();
   const randomNr = ({min = 0, max = Number.MAX_SAFE_INTEGER} = {}) =>
      Math.floor( getRandomValue() * (max - min + 1) + min );
-  const [range, map2Chrs, symRE] = [
-    (start = 0, len = 10, reMap) => reMap && reMap instanceof Function
-      ? [...Array(len)].map((_, i) => start + i).map(reMap)
-      : [...Array(len)].map((_, i) => start + i),
-    v => String.fromCharCode(v),
-    /[!?@#$%^&*=+_;\-]/g,
-  ];
+  const range = (start = 0, len = 10, reMap) => (reMap || []) instanceof Function
+    ? [...Array(len)].map((_, i) => start + i).map(reMap)
+    : [...Array(len)].map((_, i) => start + i);
+  const map2Chrs = v => String.fromCharCode(v);
+  const symRE = /[!?@#$%^&*=+_;\-]/g;
   const shuffle = array => {
     let i = array.length;
     while (i--) {
@@ -23,22 +22,24 @@ function randomStringGeneratorFactory() {
 
     return array;
   };
-  const [UC, LC] = [shuffle(range(65, 26, map2Chrs)), shuffle(range(97, 26, map2Chrs))];
+  const UC = shuffle(range(65, 26, map2Chrs));
+  const LC = shuffle(range(97, 26, map2Chrs));
   const UCLC = shuffle(UC.concat(LC));
-  const allChars = {
-    UCLC,
-    UC,
-    LC,
-    Nrs: shuffle(range()),
-    Sym: shuffle(`!@#$%^&+*=-_;?`.split(``)),
-  };
+  const allChars = { UCLC, UC, LC, Nrs: shuffle(range()), Sym: shuffle(`!@#$%^&+*=-_;?`.split(``)), };
   const getChars2Use =  use => Object.entries(use)
     .reduce( (acc, [key, value]) => value ? [...acc, ...allChars[key]] : acc, LC );
 
+  function checkBrowserCrypto() {
+    try { return window && `crypto` in window }
+    catch(_) { return false; }
+  }
+
   function uuid4() {
-    return window.crypto?.randomUUID
+    // Note: randomUUID only in secure context (https)
+    return hasCrypto && crypto.randomUUID
       ? crypto.randomUUID()
-      : [...new Uint8Array(16)].map(_ => randomNr({max: 255}))
+      : [...new Uint8Array(16)]
+          .map(_ => randomNr({max: 255}))
           .map( (v, i) => `${
             (i === 8 ? v & 0b00111111 | 0b10000000 : i === 6  ? v & 0b00001111 | 0b01000000 : v)
               .toString(16).padStart(2, `0`)}${~[3,5,7,9].indexOf(i) ? `-` : ``}` )
@@ -54,21 +55,23 @@ function randomStringGeneratorFactory() {
     let chrs2Use = shuffle( getChars2Use( { UC: includeUppercase, Nrs: includeNumbers, Sym: includeSymbols } ) );
     const rRange = shuffle(range(1, len-1));
     do { chrs2Use = [...chrs2Use, ...shuffle(chrs2Use)]; } while (chrs2Use.length < len);
-    const initial = chrs2Use.slice(0, len);
+    let initial = chrs2Use.slice(0, len).join(``);
+    const randomPos = randomNr({max: initial.length-1});
 
-    if (includeNumbers && !initial.find(v => !isNaN(parseInt(v)))) { // at least 1 number
-      initial.splice(rRange.shift(), 1, allChars.Nrs[randomNr({max: allChars.Nrs.length})]);
+    if (includeNumbers && !/\d/g.test(initial)) { // at least 1 number
+      initial = initial.slice(0, randomPos - 1) + allChars.Nrs[randomNr({max: allChars.Nrs.length-1})] + initial.slice(randomPos);
     }
 
-    if (includeSymbols && !symRE.test(initial.join(``))) { // at least 1 special char
-      initial.splice(rRange.shift(), 1, allChars.Sym[randomNr({max: allChars.Sym.length})]);
+    if (includeSymbols && !symRE.test(initial)) { // at least 1 special char
+      initial = initial.slice(0, randomPos - 1) + allChars.Sym[randomNr({max: allChars.Sym.length-1})] + initial.slice(randomPos);
     }
 
     if (startAlphabetic && !/^[a-z]/i.test(initial[0])) { // first alphabetic
-      initial.splice(0, 1, allChars.UCLC[randomNr({max: allChars.UCLC.length})]);
+      const letters = allChars[includeUppercase ? `UCLC` : `LC`];
+      initial = letters[randomNr({max: letters.length-1})] + initial.slice(1);
     }
 
-    return `${initial.join(``)}`;
+    return initial;
   }
 
   return { randomString, uuid4, };
