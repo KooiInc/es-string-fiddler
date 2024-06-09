cff4c045c228383b63764fc55d32b2450df92d9f
Renzo Kooi <renzo.kooi@gmail.com>
2024-06-09 14:23:26 +0200

    Implement interpolator (use Symbolic extension)

--- Resource/Extensions.js
+++ Resource/Extensions.js
@@ -1,8 +1,9 @@
 export default extensions;
+import dummy from "./Interpolator.js";
 
-function extensions(proxify, resolveTemplateString, {sanitize, sanitizer, silentFail} = {}) {
-  const interpolator = interpolateFactory();
-  const format = str => (...tokens) => proxify`${interpolator(str, ...tokens)}`;
+function extensions(proxify, resolveTemplateString, {sanitize, sanitizer, silentFail,} = {}) {
+  const tokenize = Symbol.for("interpolate");
+  const format = str => (...tokens) => proxify`${str[tokenize](...tokens)}`;
   const ucFirst = ([first, ...theRest]) => `${first.toUpperCase()}${theRest.join(``)}`;
   const toDashedNotation = str2Convert =>
     str2Convert.replace(/[A-Z]/g, a => `-${a.toLowerCase()}`).replace(/^-|-$/, ``);
@@ -164,26 +165,4 @@ function extensions(proxify, resolveTemplateString, {sanitize, sanitizer, silent
     indexOf,
     lastIndexOf,
   }
-}
-
-function interpolateFactory(defaultReplacer) {
-  const isStringOrNumber = v => [String, Number].find(vv => vv === Object.getPrototypeOf( v ?? {} )?.constructor);
-  const isObject = v => Object.getPrototypeOf( v ?? `` )?.constructor === Object;
-  const invalidate = key => String(defaultReplacer ?? `{${key}}`);
-  const replacement = (key, t) => !isStringOrNumber(t[key]) ? invalidate(key) : String(t[key]);
-  const replacer = token => (...args) => replacement( args.find(a => a.key).key ?? `_`, token );
-  const replace = (str, token) => str.replace( /\{(?<key>[a-z_\d]+)}/gim, replacer(token) );
-  const mergeToken = obj => {
-    const entries = Object.entries(obj);
-    const [key, values] = entries.shift();
-    const merged = values.reduce( (acc, v) => [...acc, {[key]: v}], []);
-    entries.forEach( ([key, value]) => value.forEach( (v, i) =>  merged[i][key] = `${v}`));
-    return merged;  };
-  const isMultiLineWithArrays = (...tokens) =>
-    tokens.length === 1 && Object.values(tokens[0]).filter(v => !Array.isArray(v)).length < 1;
-  const processTokens = (...tokens) => isMultiLineWithArrays(...tokens) ? mergeToken(tokens.shift()) : tokens;
-  const interpolate = (str, ...tokens) => tokens.flat()
-    .reduce( (acc, token) => acc.concat(!isObject(token) ? `` : replace(str, token )), ``);
-  
-  return (str, ...tokens) => interpolate(str, ...processTokens(...tokens));
 }
